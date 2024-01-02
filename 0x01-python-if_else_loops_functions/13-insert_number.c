500f84ff44a22950019614e9b415e1f3bfe0c4aa
nmarizani <n.marizani@alustudent.com>
2024-01-02 14:17:38 -0800

    Updated

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,34 +1,60 @@
 #include "lists.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * insert_node - Inserts a number into a sorted singly-linked list.
- * @head: A pointer the head of the linked list.
- * @number: The number to insert.
- *
- * Return: If the function fails - NULL.
- * Otherwise - a pointer to the new node.
+ * insert_node - inserts a number into sorted linked list
+ * @head: pointer to head of list
+ * @number: number to be inserted into new node
+ * Return: pointer to new node or 0 Upon Error
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-        listint_t *node = *head, *new;
-
-        new = malloc(sizeof(listint_t));
-        if (new == NULL)
-                return (NULL);
-        new->n = number;
-
-        if (node == NULL || node->n >= number)
-        {
-                new->next = node;
-                *head = new;
-                return (new);
-        }
-
-        while (node && node->next && node->next->n < number)
-                node = node->next;
-
-        new->next = node->next;
-        node->next = new;
-
-        return (new);
+listint_t *tmp = NULL;
+listint_t *new = NULL;
+if (head == NULL)
+return (NULL);
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL);
+new->n = number;
+new->next = NULL;
+/*if there is no node insert as new node */
+if (*head == NULL)
+{
+*head = new;
+(*head)->next = NULL;
+return (new);
+}
+/* if there is only one node compare and insert */
+if ((*head)->next == NULL)
+{
+if ((*head)->n < new->n)
+(*head)->next = new;
+else
+{
+new->next = *head;
+*head = new;
+}
+return (new);
+}
+/* if many nodes do comparison and insert */
+tmp = *head;
+while (tmp->next != NULL)
+{
+if (new->n < tmp->n)
+{
+new->next = tmp;
+*head = new;
+return (new);
+}
+if (((new->n > tmp->n) && (new->n < (tmp->next)->n)) || (new->n == tmp->n))
+{
+new->next = tmp->next;
+tmp->next = new;
+return (new);
+}
+tmp = tmp->next;
+}
+tmp->next = new;
+return (new);
 }
